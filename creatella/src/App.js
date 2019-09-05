7afea627a43201ecda53ce54f531af637a4a6e89
Oday Abueid <odayabueid1992m@gmail.com>
2019-09-05 16:37:12 +0300

    card

--- creatella/src/App.js
+++ creatella/src/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+
 
 const BASE_URL = "http://localhost:3000";
 
 class App extends React.Component {
   state = {
-   
+    products:[],
+    More: true
   };
 
-  // componentWillMount() {
-  //   fetch(`${BASE_URL}/api/products?_page=10&_limit=15`, {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json"
-  //     }
-  //   })
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         console.log(response)
-  //         response.json().then((json) => {
-  //           if (json.length > 0) {
-  //             this.setState(() => ({ products: json }));
-  //           } else {
-  //             this.setState({ hasMore: false });
-  //           }
-  //         });
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       this.setState(() => ({ fetchError: err }));
-  //     });
-  // }
-  
   componentWillMount(){
-    // fetch(`${BASE_URL}/api/products?_page=10&_limit=15`)
-    //   // .then(data => data.json())
-    //   .then((data) => {
-    //      console.log(data)
-    //     //  this.setState({
-    //     //     comms:data
-    //     //  })
-    //    }).catch((err)=>{
-    //     console.log(err)
-    //    })
-
     fetch(`${BASE_URL}/products`, {
       method: "GET",
       headers: {
@@ -53,13 +21,13 @@ class App extends React.Component {
       .then((response) => {
 
         if (response.ok) {
-          response.json().then((json) => {
-            console.log(json)
+          response.json().then((data) => {
+            console.log(data)
 
-            if (json.length > 0) {
-              this.setState(() => ({ products: json }));
+            if (data.length > 0) {
+              this.setState(() => ({ products: data }));
             } else {
-              this.setState({ hasMore: false });
+              this.setState({More: false });
             }
           });
         }
@@ -71,8 +39,19 @@ class App extends React.Component {
 
   render() {
     return (<div>
-        oday
-    </div>
+          {this.state.products.map((product) =>
+            <div>
+              <div className="card" style={{height:"14rem" ,width: "15rem" ,float:"left",margin:"10px", backgroundColor:"#F8F9F9 "}}>
+              <div className="card-img-top">
+              <p><span style={{fontWeight:"bold"}}></span><span style={{color:"green"}}>$ {product.price/100}</span></p>
+              <h1 style={{textAlign:"center",fontSize:`${product.size}px`}}>{product.face}</h1>
+              </div>
+              <div className="card-body">
+                <p className="card-text"><span style={{fontWeight:"bold"}}>Country:</span>{product.date}</p>
+              </div>
+              </div>
+            </div>)}
+            </div>
     )}
 }
 export default App;
